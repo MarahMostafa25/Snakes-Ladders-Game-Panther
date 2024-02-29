f1daa733c68449084539eda07974e7b9f76d91cb
Marah255 <152785397+MarahMostafa25@users.noreply.github.com>
2024-02-29 15:19:31 +0200

    Factory

--- src/designPatterns/SquareFactory.java
+++ src/designPatterns/SquareFactory.java
@@ -1,5 +1,28 @@
 package designPatterns;
 
+import model.Square;
+import model.SurpriseSquare;
+import model.QuestionSquare;
+
 public class SquareFactory {
+	
+	public Square getType(String type,int xCoordinate, int yCoordinate) {
+		if(type.equals("SurpriseSquare")) {
+			return new SurpriseSquare(xCoordinate,yCoordinate);
+		}
+		if(type.equals("Hard")) {
+			return new QuestionSquare(type,xCoordinate,yCoordinate);
+		}
+		
+		if(type.equals("Easy")) {
+			return new QuestionSquare(type,xCoordinate,yCoordinate);
+		}
+		
+		if(type.equals("Medium")) {
+			return new QuestionSquare(type,xCoordinate,yCoordinate);
+		}
+		return null;
+		
+	}
 
 }
--- src/model/QuestionSquare.java
+++ src/model/QuestionSquare.java
@@ -2,13 +2,18 @@
 package model;
 
 public class QuestionSquare extends Square{
-
-	public QuestionSquare(int xCoordinate, int yCoordinate) {
+	String type;
+	public QuestionSquare(String type,int xCoordinate, int yCoordinate) {
 		super(xCoordinate, yCoordinate);
+		this.type=type;
 		
 	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 	
-
-	
-
+			
 }
